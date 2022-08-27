f7af0248b86920abef4ed076b0f9922d0bff118e
DarkVader135 <82936530+DarkVader135@users.noreply.github.com>
2022-08-27 21:48:00 +0100

    added better functionality
    added functions where it replies and pings the person who sent the message and then deletes the original post for better embedding

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -4,16 +4,22 @@ module.exports = {
         //console.log(message.content);
         if(message.content.includes('vm.tiktok.com')){
             message.content = message.content.replace("https://vm.tiktok.com", "https://vm.dstn.to");
+            message.content = "<@" + message.author.id + "> " + message.content;
             //console.log(message.content);
             message.reply(message.content);
+            message.delete();
         } else if (message.content.includes("www.tiktok.com")){
             message.content = message.content.replace("https://www.tiktok.com", "https://vm.dstn.to");
+            message.content = "<@" + message.author.id + "> " + message.content;
             //console.log(message.content);
             message.reply(message.content);
+            message.delete();
         } else if (message.content.includes("https://twitter.com")){
             message.content = message.content.replace("https://twitter.com", "https://vxtwitter.com");
+            message.content = "<@" + message.author.id + "> " + message.content;
             //console.log(message.content);
             message.reply(message.content);
+            message.delete();
         }
     }
 }
